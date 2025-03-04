ca91f05da741c670e1d7f3226af2e7785755ffd1
c-salhab <118256379+c-salhab@users.noreply.github.com>
2025-03-04 10:05:20 +0100

    Add files via upload

---
+++ db.client.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+// database
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
+
+// authentication and synchronization
+sequelize.authenticate()
+    .then(() => {
+        sequelize.sync().catch(() => console.log("Cannot sync the database"));
+    })
+    .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+
+module.exports = sequelize;
